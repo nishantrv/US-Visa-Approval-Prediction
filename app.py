e011a366a219591607fa604407e95f6591591441
Nishant Ranjan Verma <nishantranjanverma@gmail.com>
2024-05-08 19:04:20 +0100

    data ingestion layer

---
+++ app.py
@@ -0,0 +1,4 @@
+from us_visa.pipeline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
--- demo.py
+++
@@ -1,8 +0,0 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException 
-import sys
-
-try:
-    a = 1/'10'
-except Exception as e:
-    raise USvisaException(e,sys) from e
